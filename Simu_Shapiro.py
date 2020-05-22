5316f18772618512e7ce8419b59090511c728206
bfencing98 <35990392+bfencing98@users.noreply.github.com>
2020-05-22 21:06:58 +0200

    astre devient origine du repère

--- Simu_Shapiro.py
+++ Simu_Shapiro.py
@@ -1,3 +1,10 @@
+###############################################################
+##                                                           ##
+## ISEP 2020 - Sciences du numérique - Sujet : Effet Shapiro ##
+##       Auteurs : Baptiste BONNAUDET & Martin LE GOFF       ##
+##                                                           ##
+###############################################################
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -12,15 +19,16 @@ G = 6.6738480*10**-11 #constante gravitationnelle (en m**3 kg**-1 s**-2)
 masseAstre = 1.989*10**30 #masse du soleil par défault, peut être modifié par la suite (en kg)
 rayonSchw = (2*G*masseAstre)/(c**2) #rayon de Schwarzschild (en mètres)
 vitTerre = 30 #vitesse de la Terre sur son orbite (en km/s)
-b = 1120000 #paramètre d'impact (distance la plus courte entre le centre de l'astre de le rayonnement qui l'approche) 
-r1 = 149597870 #distance Terre - Astre (en km)
-r2 = 849000000 #distance Astre - Sonde (en km)
 vitSonde = 47000 #vitesse de la sonde Cassini à partir du 30 décembre 2000 (en m/s)
 dureeObs = 30 #durée de la période d'observation (en jours) 
-coorSonde = [(0,0)]*720 #tableau de coordonnées de points appartenant à la trajectoire de la sonde
-coorSondeInit = (50000000,850000000)#coordonnées initiales de la sonde au début de la période d'observations
 freqMesure = 24 #nombre de mesures prises par jour d'observation
+distTerreAstre = 149597870 #distance Terre - Astre (en km)
+coorSonde = [[0,0]]*dureeObs*freqMesure #tableau de coordonnées de points appartenant à la trajectoire de la sonde
+coorAstre = [0,0] #coordonnées de l'astre dans un repère orthonormé dont il est l'origine 
+coorTerre = [0,-distTerreAstre] #coordonnées de la Terre si l'on considère un repère orthonormé 
+coorSondeInit = (50000000,850000000)#coordonnées initiales de la sonde au début de la période d'observations
 deltaPointMesure = ((3600*24)/freqMesure)*vitSonde/1000 #distance parcourue entre deux mesures par la sonde (en km)
+
 
 
 ################ CALCUL COORDONNEES SONDE ############################
@@ -32,7 +40,16 @@ for i in range (0,dureeObs*freqMesure):
 
 
 ######################## CALCUL RETARD SHAPIRO #######################
-retardSh = (1+gamma)*(rayonSchw/c)*(np.log((4*r1*r2)/(b**2))+1)
+#d'après la formule pour un aller-retour à proximité d'un astre massif
+retardSh = [0]*len(coorSonde)
+
+for x in len(coorSonde):
+    r1 = np.sqrt((coorAstre[0]-coorTerre[0])**2+(coorAstre[1]-coorTerre[0])**2) #distance Terre - Astre (en km)
+    r2 = np.sqrt((coorSonde[x][0]-coorAstre[0])**2+(coorSonde[x][1]-coorAstre[1])**2) #distance Astre - Sonde (en km)
+
+    b = 1120000 #paramètre d'impact (distance la plus courte entre le centre de l'astre de le rayonnement qui l'approche)
+
+    retard[x] = (1+gamma)*(rayonSchw/c)*(np.log((4*r1*r2)/(b**2))+1)
 
 
 ##################### CALCUL DECALAGE FREQUENTIEL ####################
