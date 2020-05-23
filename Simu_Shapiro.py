4729dc2e1405a572d72faf45b181217bfe445ed0
bfencing98 <35990392+bfencing98@users.noreply.github.com>
2020-05-23 02:05:49 +0200

    vrais progrès
    La courbe est produite, mais le paramètre b est 1000 fois trop petit... le décalage fréquentiel observé est donc 1000 fois trop grand.
    Probablement une erreur dans une formule quelque part

--- Simu_Shapiro.py
+++ Simu_Shapiro.py
@@ -8,9 +8,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = [0.25, 0.25, 1.25, 0.5, 1, 0.25, 0.6, 0, -0.6, -0.25, -1, -0.5, -1.25, -0.25, -0.25, 0.25]
-y = [0, 0.5, 0.5, 1, 1, 1.5, 1.5, 2, 1.5 , 1.5, 1, 1, 0.5, 0.5, 0, 0]
-
 
 ############################### VARIABLES ############################## (toutes les distances sont en kilomètres)
 gamma = 1 #constante de RG
@@ -21,57 +18,68 @@ rayonSchw = (2*G*masseAstre)/(c**2) #rayon de Schwarzschild (en mètres)
 vitTerre = 30 #vitesse de la Terre sur son orbite (en km/s)
 vitSonde = 47000 #vitesse de la sonde Cassini à partir du 30 décembre 2000 (en m/s)
 dureeObs = 30 #durée de la période d'observation (en jours) 
-freqMesure = 24 #nombre de mesures prises par jour d'observation
+freqMesure = 96 #nombre de mesures prises par jour d'observation
 distTerreAstre = 149597870 #distance Terre - Astre (en km)
 coorSonde = [[0,0]]*dureeObs*freqMesure #tableau de coordonnées de points appartenant à la trajectoire de la sonde
 coorAstre = [0,0] #coordonnées de l'astre dans un repère orthonormé dont il est l'origine 
 coorTerre = [0,-distTerreAstre] #coordonnées de la Terre si l'on considère un repère orthonormé 
 coorSondeInit = (50000000,850000000)#coordonnées initiales de la sonde au début de la période d'observations
 deltaPointMesure = ((3600*24)/freqMesure)*vitSonde/1000 #distance parcourue entre deux mesures par la sonde (en km)
-
+retardSh = [0]*len(coorSonde)
+freqShift = [0]*len(coorSonde)
+xAxe = [0]*len(coorSonde)
 
 
 ################ CALCUL COORDONNEES SONDE ############################
-for i in range (0,dureeObs*freqMesure):
+for i in range (0,len(coorSonde)):
     newCoor = [0,0]
     newCoor[0] = coorSondeInit[0]-(i*np.sqrt((deltaPointMesure**2)/2))
     newCoor[1] = coorSondeInit[1]+(i*np.sqrt((deltaPointMesure**2)/2))
     coorSonde[i] = newCoor
+    xAxe[i] = i*(dureeObs/(dureeObs*freqMesure))
 
 
-######################## CALCUL RETARD SHAPIRO #######################
-#d'après la formule pour un aller-retour à proximité d'un astre massif
-retardSh = [0]*len(coorSonde)
+for x in range (0,len(coorSonde)):#
 
-for x in len(coorSonde):
-    r1 = np.sqrt((coorAstre[0]-coorTerre[0])**2+(coorAstre[1]-coorTerre[0])**2) #distance Terre - Astre (en km)
+    ########################## CALCUL PARAMETRES #####################
+    r1 = np.sqrt((coorAstre[0]-coorTerre[0])**2+(coorAstre[1]-coorTerre[1])**2) #distance Terre - Astre (en km)
     r2 = np.sqrt((coorSonde[x][0]-coorAstre[0])**2+(coorSonde[x][1]-coorAstre[1])**2) #distance Astre - Sonde (en km)
+    vectDir = (coorSonde[x][0]-coorTerre[0],coorSonde[x][1]-coorTerre[1])
+    coeffA =  vectDir[1]
+    coeffB = -1*vectDir[0]
+    coeffC = coorTerre[0]*vectDir[1]+coorTerre[1]*vectDir[0]
+    b = -1*((coeffA*coorAstre[0])+(coeffB*coorAstre[1])+(coeffC))/(np.sqrt((coeffA**2)+(coeffB**2)))
+    #b = 1120000 #paramètre d'impact (distance la plus courte entre le centre de l'astre de le rayonnement qui l'approche)
 
-    b = 1120000 #paramètre d'impact (distance la plus courte entre le centre de l'astre de le rayonnement qui l'approche)
 
-    retard[x] = (1+gamma)*(rayonSchw/c)*(np.log((4*r1*r2)/(b**2))+1)
+    ######################## CALCUL RETARD SHAPIRO ###################
+    #d'après la formule pour un aller-retour à proximité d'un astre massif
+    retardSh[x] = (1+gamma)*(rayonSchw/c)*(np.log((4*r1*r2)/(b**2))+1)
 
 
-##################### CALCUL DECALAGE FREQUENTIEL ####################
-freqShift = -(rayonSchw/c)*(1+gamma)*(1/b)*vitTerre
+    ##################### CALCUL DECALAGE FREQUENTIEL ####################
+    freqShift[x] = -(rayonSchw/c)*(1+gamma)*(1/b)*vitTerre
+
+    if freqShift[x] > freqShift[x-1] :
+        bMin = b
 
 
 ##################### AFFICHAGE DES RESULTATS ########################
 print("Masse de l'astre : "+str(masseAstre))
 print("Rayon de Schwarzschild : "+str(rayonSchw))
-print("Retard Shapiro : "+str(retardSh))
-print("Décalage fréquentiel : "+str(freqShift))
+#print("Retard Shapiro : "+str(retardSh))
+#print("Décalage fréquentiel : "+str(freqShift))
 print("Distance entre mesures : "+str(deltaPointMesure))
 print("Coordonnées point de mesure 1 : ("+str(coorSonde[0][0])+","+str(coorSonde[0][1])+")")
 print("Coordonnées point de mesure 2 : ("+str(coorSonde[1][0])+","+str(coorSonde[1][1])+")")
 print("Distance parcourue par la sonde au cours de la période d'observation : "+str(dureeObs*24*3600*vitSonde/1000)+" km")
 print("Distance entre le premier et le dernier point de mesure : "+str(np.sqrt(((coorSonde[719][0]-coorSondeInit[0])**2+(coorSonde[719][1]-coorSondeInit[1])**2)))+" km")
+print("paramètre bMin : "+str(bMin))
 
-#plt.plot(x, y, '-.', color = "green", lw = 2)
-#plt.title("Mon beau sapin")
-#plt.axis('equal')
-#plt.xlabel("C'est Noel")
-#plt.ylabel("Vive le vent")
-#plt.show()
-#plt.close()
+plt.plot(xAxe, freqShift, '-', color = "blue", lw = 1)
+plt.title("Décalage fréquentiel subi aux alentours d'un astre massif\n")
+plt.xlabel("Jours d'observations")
+plt.ylabel("Décalage fréquentiel")
+plt.show()
+plt.close()
 
